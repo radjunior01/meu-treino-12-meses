3f58fe1736b3f7895581f4f4647aecbc95648dfc
radjunior01 <59528833+radjunior01@users.noreply.github.com>
2026-08-21 16:10:15 -0300

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.radjunior.meutreino',
+  appName: 'Meu Treino',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
