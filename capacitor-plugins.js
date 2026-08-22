6e4134ecfe36108160b251b133c6caa8afd4170b
radjunior01 <59528833+radjunior01@users.noreply.github.com>
2026-08-22 16:55:30 -0300

    Add Capacitor plugins for Filesystem and Share

---
+++ capacitor-plugins.js
@@ -0,0 +1,8 @@
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+
+window.CapacitorPlugins = {
+    Filesystem,
+    Directory,
+    Share
+};
