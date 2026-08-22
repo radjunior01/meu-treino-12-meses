5e9df393f6ac8b805c83d266c93d622df0019e38
radjunior01 <59528833+radjunior01@users.noreply.github.com>
2026-08-22 17:02:35 -0300

    Delete capacitor-plugins.js

--- capacitor-plugins.js
+++
@@ -1,8 +0,0 @@
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Share } from '@capacitor/share';
-
-window.CapacitorPlugins = {
-    Filesystem,
-    Directory,
-    Share
-};
